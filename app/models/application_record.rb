e397b000e507e1c860ad7eb45f723ec1f03a8aa0
Breinich <vencel@bajnok.hu>
2021-10-25 20:49:43 +0200

    Second Commit

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+
+def start_time
+  self.date
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'say/hello'
+  get 'users/new'
+  get 'weblap/main'
+  get 'weblap/shop'
+  get 'weblap/advice'
+  get 'weblap/cart'
+  get 'weblap/order'
+  get 'weblap/main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
